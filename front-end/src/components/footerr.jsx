025407b8fb442431e97ae568bfb50b4660384795
Mahsa Aghaie <mahsa.mae14@gmail.com>
2023-04-01 01:17:25 +0430

    edit footer

--- front-end/src/components/footerr.jsx
+++ front-end/src/components/footerr.jsx
@@ -1,134 +1,77 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
-import email from '../images/email.svg'
 import logo from '../images/logo.png'
-import telegram from '../images/telegram.svg'
 export default function Footer() {
   return (
    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted fixed-bottom' id="footerstyle" dir='rtl'>
-      {/* <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'> */}
-        {/* <div className='me-5 d-none d-lg-block'>
-          <span>در شبکه های اجتماعی با ما در ارتباط باشید:</span>
-        </div>
-        <div>
-          <a href='' className='fab fa-youtube'>
-            {/* <MDBIcon color='secondary' className="fab fa-youtube" /> */}
-            {/* <img src={email} alt="http:\\google.com" id='image-email' title='ایمیل'></img>
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='twitter' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='google' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='instagram' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='linkedin' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='github' />
-          </a>
-        </div> */}
-      {/* </section> */}
-
       <section className=''>
-        <MDBContainer className='text-center text-md-start mt-5'>
+        <MDBContainer className='text-center  mt-5 text-start'>
           <MDBRow className='mt-3'>
             <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
               <img src={logo} alt="" id='logo'/>
               <p>سایت مشاوره و خدمات روانشناسی psytech</p>
-              <img src={telegram} alt="" id='img-under-logo' />
-              <img src={email} alt="" id='img-under-logo'/>
-              <img src={email} alt="" id='img-under-logo'/>
-              <img src={telegram} alt="" id='img-under-logo'/>
-              {/* <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon color='secondary' icon='gem' className='me-3' />
-                Company name
-              </h6> */}
-              {/* <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit
-                amet, consectetur adipisicing elit.
-              </p> */}
+              <ul>
+              <li className='icone-under-logo'><span><i className="fa-solid fa-phone"></i></span></li>
+                <li className='icone-under-logo'><span><i className="fa-brands fa-telegram" /></span></li>
+                <li className='icone-under-logo'><span> <i className="fa-solid fa-envelope" /></span> </li>
+                <li className='icone-under-logo'> <span><i className="fa-brands fa-instagram" /></span></li>
+              </ul>
             </MDBCol>
-
+            <MDBCol md='4' lg='3' xl='3' className='mx-auto  mb-md-0 mb-4' dir='rtl'>
+              <h6 className='text-uppercase fw-bold mb-3' dir='rtl'>پشتیبانی</h6>
+              <p dir='rtl'>
+                <MDBIcon color='secondary' icon='home' className='me-1' />
+                <span> اصفهان، دانشگاه اصفهان   </span>
+                
+              </p>
+              <p>
+              <MDBIcon color='secondary' icon='envelope' className='me-2' />
+                <span> psytech@gmail.com  </span>
+              </p>
+              <p dir='rtl'>
+                <MDBIcon color='secondary' icon='phone' className='me-3' dir='rtl'/>
+               <span dir='rtl'>  0936-666-6666   </span>   
+              </p>
+            </MDBCol>
             <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>خدمات سایت </h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  مشاوره خانواده
-                </a>
+                <a href='#!' className='text-reset a-non-decoration'>مشاوره خانواده</a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  مشاوره فردی
-                </a>
+                <a href='#!' className='text-reset a-non-decoration'> مشاوره فردی </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  مشاوره تحصیلی
-                </a>
+                <a href='#!' className='text-reset a-non-decoration'>مشاوره تحصیلی</a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
+                <a href='#!' className='text-reset a-non-decoration'>
                   مشاوره ازدواج
                 </a>
               </p>
             </MDBCol>
-
             <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
+                <a href='#!' className='text-reset fw-bold a-non-decoration'>
+                  درباره ما
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Settings
+                <a href='#!' className='text-reset fw-bold a-non-decoration'>
+                  قوانین
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Orders
+                <a href='#!' className='text-reset fw-bold a-non-decoration'>
+                 گزارش تخلف
                 </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
-                </a>
-              </p>
+              </p>  
             </MDBCol>
 
-            <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <MDBIcon color='secondary' icon='home' className='me-2' />
-                New York, NY 10012, US
-              </p>
-              <p>
-                <MDBIcon color='secondary' icon={email} className='me-3' />
-                {/* <img src={email} alt="" id='email-image-context'/> */}
-                info@example.com
-              </p>
-              <p>
-                <MDBIcon color='secondary' icon='phone' className='me-3' /> + 01 234 567 88
-              </p>
-              <p>
-                <MDBIcon color='secondary' icon='print' className='me-3' /> + 01 234 567 89
-              </p>
-            </MDBCol>
+            
           </MDBRow>
         </MDBContainer>
       </section>
-
-      {/* { <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2021 Copyright:
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
-      </div>} */}
     </MDBFooter>
   );
 }
