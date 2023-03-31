1a25afb82434cf7a040f1ca279446d70e3196984
Mahsa Aghaie <mahsa.mae14@gmail.com>
2023-03-31 22:03:58 +0430

    basic structure
    basic structure navbar and footer- setting react-bootstrap-MDB-tailwind

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBarr from "./components/navBarr";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/footerr"
+function App()
+{
+    return (
+        <>
+        <NavBarr></NavBarr>
+    <Footer></Footer>
+        </>
+
+)
+ }
+export default App;
