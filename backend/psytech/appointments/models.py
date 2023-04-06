e2bcd1e114808aa08455cf9c4ffb4857fd016480
faz-7 <fasa.pub@gmail.com>
2023-04-06 21:29:37 +0430

    create models of appointments app

--- backend/psytech/appointments/models.py
+++ backend/psytech/appointments/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Schedule(models.Model):
+    date = models.DateField(auto_now_add=True)
+    start = models.TimeField(blank=True)
+    end = models.TimeField(blank=True)
+
+    class Meta:
+        ordering = ('date',)
+
+    def __str__(self):
+        return f'{self.date}'
+
+
+class Appointment(models.Model):
+    start = models.TimeField()
+    end = models.TimeField()
+    reserved = models.BooleanField()
+    display = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.start} - {self.end}'
+
+
+class Booking(models.Model):
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE, related_name='appointments')
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patients')
+    psychologist = models.ForeignKey(User, on_delete=models.CASCADE, related_name='psychologists')
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.date} - {self.psychologist} - {self.patient}'
+
+
+class Bill(models.Model):
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='booking')
+    amount = models.FloatField()
+    status = models.BooleanField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.date} - {self.booking}'
