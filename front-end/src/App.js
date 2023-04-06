5e84fa1df9a7f00340503b21167916830818f0f6
Mahsa Aghaie <mahsa.mae14@gmail.com>
2023-04-07 02:39:06 +0430

    dr.slider

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./components/footerr";
 import './App.css';
 import './components/footerStyle.css'
-
+import DoctorSlider from './components/doctorSlider'
+import './components/doctorSlider.css'
 function App()
 {
     return (
         <>
         <NavBarr></NavBarr>
+        <DoctorSlider></DoctorSlider>
        <Footer></Footer>
         </>
 
