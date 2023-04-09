f7b82d0ca0cf5f5d67949d7241dbea1a0ad51dcf
AA00163 <AAlimohammadi00163@gmail.com>
2023-04-09 16:41:20 -0700

    add view register,login

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.shortcuts import render, redirect
+from pyexpat.errors import messages
+from django.urls import reverse, reverse_lazy
 
-# Create your views here.
+from backend.accounts.forms import UserRegistrationForm, UserLoginForm
+
+class HomeView(View):
+    template_name = 'product/index.html'
+
+    def get(self, request, category_slug=None):
+
+        return render(request, self.template_name, {})
+
+    def post(self, request):
+        return render(request, self.template_name)
+class UserRegisterView(View):
+    template_name = 'accounts/register.html'
+
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, self.template_name, {'form': form})
+
+
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('product/home.html')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, phone_number=cd['phone'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                if user.is_staff():
+                    return redirect(to=reverse('admin:index'))
+                else:
+                    messages.success(request, 'you logged in successfully', 'info')
+                    return redirect('product:home')
+            messages.error(request, 'phone or password is wrong', 'warning')
+        return render(request, self.template_name, {'form': form})
+
+
+class UserLogoutView(LoginRequiredMixin, View):
+    login_url = '/accounts/login/'
+
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'you logged out successfully', 'success')
+        return redirect('product:home')
