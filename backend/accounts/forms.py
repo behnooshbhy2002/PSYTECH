ed0b3995abe2a32520346062cf0c8ae4edaf7d21
faz-7 <fasa.pub@gmail.com>
2023-04-09 18:10:08 +0430

    Update database

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -2,37 +2,36 @@ from .models import User
 from django import forms
 from django.core.exceptions import ValidationError
 
+
 class UserRegistrationForm(forms.Form):
     full_name = forms.CharField(widget=forms.TextInput(attrs={'class:': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class:': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     phone = forms.CharField(max_length=11)
-
-    # password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     def clean_email(self):
         email = self.cleaned_data['email']
         user = User.objects.filter(email=email).exists()
         if user:
-            raise ValidationError('this email is already exist')
+            raise ValidationError('email already exists')
         return email
 
-    # def clean_phone(self):
-    #     phone = self.cleaned_data['phone']
-    #     user = User.objects.filter(phone_number=phone).exists()
-    #     if user:
-    #         raise ValidationError('this phone number is already exist')
-    #     OtpCode.objects.filter(phone_number=phone).delete()
-    #     return phone
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone_number')
+        user = User.objects.filter(phone_number=phone).exists()
+        if user:
+            raise ValidationError('Phone number already exists')
+        return phone
 
-    # def clean(self):
-    #     cd = super().clean()
-    #     p1 = cd.get('password1')
-    #     p2 = cd.get('password2')
-    #     if p1 and p2 and p1 != p2:
-    #         raise ValidationError('password must match')
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password and password:
+            raise ValidationError('confirm password does not match password')
+        return confirm_password
 
 
 class UserLoginForm(forms.Form):
-    email = forms.CharField(widget=forms.TextInput(attrs={'class:': 'form-control'}))
+    email = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
