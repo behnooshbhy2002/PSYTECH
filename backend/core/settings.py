4b665e3fa0eb393b27ed7ba8521ef0846aad513a
AA00163 <AAlimohammadi00163@gmail.com>
2023-04-09 16:41:20 -0700

    add view register,login

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'psytech',
         'USER': 'postgres',
-        'PASSWORD': 'mithri74',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
