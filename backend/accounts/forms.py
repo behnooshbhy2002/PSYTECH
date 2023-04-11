f8d96e116ea4d1ed21c4691811226dbc828c4066
AA00163 <AAlimohammadi00163@gmail.com>
2023-04-11 01:23:09 -0700

    add register form and front of register

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -3,12 +3,46 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
-class UserRegistrationForm(forms.Form):
-    full_name = forms.CharField(widget=forms.TextInput(attrs={'class:': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class:': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    phone = forms.CharField(max_length=11)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+class PsychologistRegistrationForm(forms.Form):
+    full_name = forms.CharField(help_text='نام و نام خانوادگی')
+    phone = forms.CharField(help_text='شماره تلفن همراه')
+    email = forms.EmailField(help_text='ایمیل')
+    gender = forms.CharField(help_text='جنسیت')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), help_text='رمز')
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                       help_text='تکرار رمز')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('email already exists')
+        return email
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone_number')
+        user = User.objects.filter(phone_number=phone).exists()
+        if user:
+            raise ValidationError('Phone number already exists')
+        return phone
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password and password:
+            raise ValidationError('confirm password does not match password')
+        return confirm_password
+
+
+class PatientRegistrationForm(forms.Form):
+    full_name = forms.CharField(help_text='نام و نام خانوادگی')
+    phone = forms.CharField(help_text='شماره تلفن همراه')
+    email = forms.EmailField(help_text='ایمیل')
+    gender = forms.CharField(help_text='جنسیت')
+    id_psychologist = forms.CharField(help_text='شماره نظام پزشکی')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), help_text='رمز')
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                       help_text='تکرار رمز')
 
     def clean_email(self):
         email = self.cleaned_data['email']
