677bfcb791245b32444d1cb64b889b66b3634ffd
behnooshbhy2002 <behnoosh.bhy@gmail.com>
2023-05-01 23:38:03 +0430

    put faz2 component together and fix probable bugs

---
+++ front-end/src/Components/MainPage/navBarr.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../images/logo.png";
+import "../style/navBar.css";
+
+const NavBarr = () => {
+  return (
+    <div className="navbar-div" dir="rtl">
+      <ul>
+        <img src={logo} className="navbar-logo" />
+        <li className="list">خانه</li>
+        <li className="list">لیست مشاوران</li>
+        <li className="list">درباره ما</li>
+        <li className="list">تماس با ما</li>
+        <button id="signup">ثبت نام</button>
+        <button id="login">ورود</button>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBarr;
