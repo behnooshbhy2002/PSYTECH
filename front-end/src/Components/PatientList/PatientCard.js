4d8c027f4dd141633cbf1f5ad4e21f54aee3477a
Mahsa Aghaie <mahsa.mae14@gmail.com>
2023-05-05 15:37:54 +0430

    patient-list-static and add more button

--- front-end/src/Components/PatientList/PatientCard.js
+++ front-end/src/Components/PatientList/PatientCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import women from '../../images/woman.png'
 import '../style/PatientCard.css'
-
-const PatientCard = () => {
-  let gender="women";
+const PatientCard = (props) => {
+  let{image,name}=props.data;
   return (
     <div className="patient-card">
-          <div className=" patient-card-img">
-        <img src={women} alt="women" height="110" width="110" className='patient-card-img'/>
+      <span className='col-md-9 patient-details'>
+      <div >
+        <img src={image} alt="women" height="110" width="110" className='patient-card-img'/>
       </div>
       <div className=" patient-card-name">
-        <h4 className='patient-card-name'>پناه راستی</h4>
+        <h4 className=' patient-card-name'>{name}</h4>
       </div>
+      </span>
       <div>
         <button className='patient-card-button'> 
         مشاهده و تکمیل پرونده</button>
--- front-end/src/Components/PatientList/PatientsList.js
+++ front-end/src/Components/PatientList/PatientsList.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
 import PatientCard from './PatientCard';
 import { useState, useEffect } from "react";
-class PatientsList extends Component {
+function PatientsList() {
+  const [drList, setDrList] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3001/list");
 
-  render() {
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setDrList(json));
+  }, [url]);
+  
+  const DrArr_lenght = drList.length;
+  console.log(DrArr_lenght);
+  const [isShowMore, setIsShowMore] = useState(true);
+  const toggleReadMoreLess = () => {
+    setIsShowMore(!isShowMore);
+  };
+
+  const numberOfItems = !isShowMore ? drList.length : 10;
+  numberOfItems < 10 && setIsShowMore(!isShowMore);
+
     return (
+      <>
       <div className="container patient-list">
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
-        <PatientCard />
+          <div>
+            {drList.slice(0, numberOfItems).map((item) => {
+              return (   
+                  <PatientCard
+                    data={{
+                      image: item.picture,
+                      name: item.name,
+                    }}
+                  />
+            
+              );
+            })}
+          </div>
+       
       </div>
+      <div>
+        {isShowMore && (
+          <button onClick={toggleReadMoreLess} className="showAllDrBtn">
+            <span className="showAllDrBtn__show_all">
+              {isShowMore && "بیشتر"}
+            </span>
+          </button>
+        )}
+      </div>
+      </>
     );
   }
-}
 
 export default PatientsList;
