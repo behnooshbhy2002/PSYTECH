5ac2e237512667ba20cc6e816759eecfad86485d
faz-7 <fasa.pub@gmail.com>
2023-05-07 00:21:43 +0430

    add login API view

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from rest_framework.authtoken import views as auth_token
 
-name_space = 'accounts'
+app_name = 'accounts'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('api-token-auth/', auth_token.obtain_auth_token),
@@ -10,3 +10,4 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
 ]
+# todo: register url for psychologist
