e70ac9a669ad3e63aefd37589873561abb176943
Hasti <hasti@email.com>
2023-05-21 12:10:29 +0330

    added test for logout, token, refresh token and verify

--- backend/accounts/tests/test_auth.py
+++ backend/accounts/tests/test_auth.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -44,3 +45,75 @@ class TestAuth(TestCase):
         res = self.client.post(url, data)
         self.assertTrue(res.status_code == 400)
         self.assertIn("email", res.data)
+
+    def test_logout_success(self):
+        url = "/accounts/login/"
+        data = {
+            "email": self.email,
+            "password": self.password,
+        }
+        res = self.client.post(url, data)
+        self.assertTrue(res.status_code == 200)
+        user = auth.get_user(self.client)
+        assert user.is_authenticated
+
+    def test_logout_failed(self):
+        user = auth.get_user(self.client)
+        assert not user.is_authenticated
+
+    def test_get_token_success(self):
+        url = "/accounts/token/"
+        data = {
+            "email": self.email,
+            "password": self.password,
+        }
+        res = self.client.post(url, data)
+        self.assertTrue(res.status_code == 200)
+        self.assertIn("access", res.data)
+        self.assertIn("refresh", res.data)
+
+    def test_get_token_failed(self):
+        url = "/accounts/token/"
+        data = {
+            "email": "invalid email",
+            "password": self.password,
+        }
+        res = self.client.post(url, data)
+        self.assertTrue(res.status_code == 401)
+        self.assertNotIn("access", res.data)
+        self.assertNotIn("refresh", res.data)
+
+    def test_refresh_token_success(self):
+        url = "/accounts/token/"
+        data = {
+            "email": self.email,
+            "password": self.password,
+        }
+        res = self.client.post(url, data)
+        self.assertTrue(res.status_code == 200)
+        self.assertIn("access", res.data)
+        self.assertIn("refresh", res.data)
+
+        url = "/accounts/token/refresh/"
+        data = {
+            "refresh": res.data['refresh'],
+        }
+        res = self.client.post(url, data)
+        self.assertTrue(res.status_code == 200)
+
+    def test_refresh_token_failed(self):
+        url = "/accounts/token/refresh/"
+        data = {
+            "refresh": "asdsadsdsadsa",
+        }
+        res = self.client.post(url, data)
+        self.assertTrue(res.status_code == 401)
+
+    def test_verify_success(self):
+        url = "/accounts/verify/"
+        data = {
+            "email": self.email,
+            "otp": "otp",
+        }
+        res = self.client.post(url, data)
+        self.assertTrue(res.status_code == 200)
