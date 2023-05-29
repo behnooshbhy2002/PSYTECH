679188d2ccb45bfb567ccafe4e31f076f90291b0
faz-7 <fasa.pub@gmail.com>
2023-05-29 15:55:20 +0330

    add patient list

--- backend/appointments/views.py
+++ backend/appointments/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from accounts.models import Psychologist, User
+from accounts.models import Psychologist, Patient
 from accounts.serializers import ActivePsychologistSerializer
 from appointments.models import Request
 from appointments.serializers import RequestSerializer, PostRequestSerializer
@@ -18,6 +18,28 @@ class RequestListView(APIView):
         psychologist = Psychologist.objects.get(id=id_dr)
         requests_lists = Request.objects.filter(receiver=psychologist)
         request_serializer = RequestSerializer(requests_lists, many=True)
+        return Response(request_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PostRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            accept_status = serializer.data.get('accept_status')
+            pk = serializer.data.get('pk')
+            request = Request.objects.get(pk=pk)
+            print(accept_status, pk)
+            request.accept_status = accept_status
+            request.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PatientList(APIView):
+
+    def get(self, request):
+        id_dr = request.query_params.get('id')
+        psychologist = Psychologist.objects.get(id=id_dr)
+        patient_lists = psychologist.patients
+        patient_serializer = Patient(requests_lists, many=True)
         return Response(request_serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
