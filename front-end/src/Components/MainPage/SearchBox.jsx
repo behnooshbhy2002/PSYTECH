7ee2fd5bebe206c6b7fee6a8e950184a90a02e3b
behnooshbhy2002 <behnoosh.bhy@gmail.com>
2023-05-29 12:15:11 +0330

    search connection

--- front-end/src/Components/MainPage/SearchBox.jsx
+++ front-end/src/Components/MainPage/SearchBox.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "../style/SearchBox.css";
 import therapy from "../../images/therapy.png";
+
 const SearchBox = () => {
+  const [keyword, setKeyword] = useState();
+
+  const history = useNavigate();
+
+  const addQuery = (key, value, url) => {
+    let searchParams = new URLSearchParams(url.search);
+    searchParams.set(key, value);
+    url({
+      pathname: "/PsycologistList",
+      search: searchParams.toString(),
+    });
+  };
+
+  const removeQuery = (key, url) => {
+    let searchParams = new URLSearchParams(url.search);
+    searchParams.delete(key);
+    url({
+      pathname: "/PsycologistList",
+      search: searchParams.toString(),
+    });
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      console.log(keyword);
+      history("/PsycologistList");
+      removeQuery("keyword", history);
+      addQuery("keyword", keyword, history);
+    }
+  };
   return (
     <div className="contain">
       <img src={therapy} className="pic" />
@@ -17,6 +49,11 @@ const SearchBox = () => {
             className="text"
             placeholder="جستجو کنید...."
             dir="rtl"
+            value={keyword}
+            onChange={(e) => {
+              setKeyword(e.target.value);
+            }}
+            onKeyDown={handleKeyDown}
           ></input>
         </div>
       </div>
