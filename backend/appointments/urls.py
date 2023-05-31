6a380f1da4573ad053535719dbf61553d1ba7458
AA00163 <AAlimohammadi00163@gmail.com>
2023-05-30 17:23:15 -0700

    add psychologists detail view

--- backend/appointments/urls.py
+++ backend/appointments/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'appointments'
 urlpatterns = [
     path('request_list/', views.RequestListView.as_view(), name='request_list'),
+    path('psychologist_detail/', views.ShowPsychologistDetailView.as_view(), name='psychologist_detail'),
 ]
