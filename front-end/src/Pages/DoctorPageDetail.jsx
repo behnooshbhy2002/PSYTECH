f3c0669c0d3b0928edce69f255002becf53cdd22
behnooshbhy2002 <behnoosh.bhy@gmail.com>
2023-06-01 14:21:32 +0330

    pop up code

--- front-end/src/Pages/DoctorPageDetail.jsx
+++ front-end/src/Pages/DoctorPageDetail.jsx
@@ -8,6 +8,12 @@ import axios from "axios";
 import { useLocation } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { DrDetails } from "../actions/doctorActions";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 function DoctorPageDetail() {
   const [psyInfo, setPsyInfo] = useState({});
   const [sickness, setSickness] = useState([]);
@@ -31,54 +37,56 @@ function DoctorPageDetail() {
   const dr = useSelector((state) => state.drDetails);
   const { error, loading, details } = dr;
   //console.log(error)
-  let image , name , address , exprience , rate , id , medical , phone;
-  let diseaseArr= []
+  let image, name, address, exprience, rate, id, medical, phone;
+  let diseaseArr = [];
 
-  
-     console.log(Array.isArray(details))
-     console.log("out")
-     if(!Array.isArray(details)){
-       console.log("gol is here")
-      //const { psychologist : [{name , gender}]} = details
-      const { psychologist, disease } = details;
-      if(Array.isArray(psychologist))
-      {
-        console.log((psychologist[0]))
-        image = psychologist[0].image
-        name = psychologist[0].full_name
-        address = psychologist[0].address
-        exprience = psychologist[0].exprience
-        //rate = psychologist[0].
-        medical = psychologist[0].medical_number
-        phone = psychologist[0].phone_number
-        //console.log(image , name)
-      }
-      if(Array.isArray(disease)){
-        diseaseArr = disease
-        console.log(diseaseArr)
-      }
-       
-     }
-     
-    //console.log(Object.keys(details));
-    // console.log(psyInfo);
-    //const x = psyInfo['0'];
-    //console.log(x)
-    // const data = details.json()
-    //const data = JSON.stringify(details)
-    //const data2 = JSON.parse(data)
-    //const {dieseas} = data2
-    // const d2 = JSON.parse( JSON.stringify(disease))
-    //console.log( typeof JSON.stringify(disease))
-    //const {khar} = psychologist
-  
-  
+  console.log(Array.isArray(details));
+  console.log("out");
+  if (!Array.isArray(details)) {
+    console.log("gol is here");
+    //const { psychologist : [{name , gender}]} = details
+    const { psychologist, disease } = details;
+    if (Array.isArray(psychologist)) {
+      console.log(psychologist[0]);
+      image = psychologist[0].image;
+      name = psychologist[0].full_name;
+      address = psychologist[0].address;
+      exprience = psychologist[0].exprience;
+      //rate = psychologist[0].
+      medical = psychologist[0].medical_number;
+      phone = psychologist[0].phone_number;
+      //console.log(image , name)
+    }
+    if (Array.isArray(disease)) {
+      diseaseArr = disease;
+      console.log(diseaseArr);
+    }
+  }
+
+  const [open, setOpen] = React.useState(false);
+  const handleCommentButton = () => {};
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  //console.log(Object.keys(details));
+  // console.log(psyInfo);
+  //const x = psyInfo['0'];
+  //console.log(x)
+  // const data = details.json()
+  //const data = JSON.stringify(details)
+  //const data2 = JSON.parse(data)
+  //const {dieseas} = data2
+  // const d2 = JSON.parse( JSON.stringify(disease))
+  //console.log( typeof JSON.stringify(disease))
+  //const {khar} = psychologist
+
   //const x = psychologist
   // console.log(x)
   // console.log(x.image)
-  
-
-  
 
   // setPsyInfo(psychologist[0]);
   // setSickness(disease);
@@ -88,106 +96,128 @@ function DoctorPageDetail() {
 
   return (
     <>
-            <div className="detail-doctor-top-page">
-              <div className="main-datail-doctor ">
-                <img
-                  src={image}
-                  alt=""
-                  className="img-fluid mx-auto d-block rounded-circle main-detail-doctor-img"
-                />
-                <div className="main-detail-doctor-text">
-                  <h4>{name}</h4>
-                  <h5>
-                    میزان تجربه:
-                    {exprience}
-                  </h5>
-                  <p> شماره نظام پزشکی: {medical}</p>
-                </div>
-              </div>
+      <div className="detail-doctor-top-page">
+        <div className="main-datail-doctor ">
+          <img
+            src={image}
+            alt=""
+            className="img-fluid mx-auto d-block rounded-circle main-detail-doctor-img"
+          />
+          <div className="main-detail-doctor-text">
+            <h4>{name}</h4>
+            <h5>
+              میزان تجربه:
+              {exprience}
+            </h5>
+            <p> شماره نظام پزشکی: {medical}</p>
+          </div>
+        </div>
 
-              <div
-                className="request-doctor-detail"
-                style={{ backgroundColor: "white" }}
-              >
-                <h5 className="loc-h5-text">آدرس مطب و شماره تماس</h5>
-                <div className="loc-div">
-                  <LocationOnIcon className="loc-icon"></LocationOnIcon>
-                  {address}
-                </div>
-                <div className="loc-div-buttom loc-div">
-                  <LocalPhoneIcon className="loc-icon loc-icon-phone"></LocalPhoneIcon>
-                  {phone}
-                </div>
-              </div>
+        <div
+          className="request-doctor-detail"
+          style={{ backgroundColor: "white" }}
+        >
+          <h5 className="loc-h5-text">آدرس مطب و شماره تماس</h5>
+          <div className="loc-div">
+            <LocationOnIcon className="loc-icon"></LocationOnIcon>
+            {address}
+          </div>
+          <div className="loc-div-buttom loc-div">
+            <LocalPhoneIcon className="loc-icon loc-icon-phone"></LocalPhoneIcon>
+            {phone}
+          </div>
+        </div>
+      </div>
+
+      <div className="detail-docor-buttom-page">
+        <div className="detail-docor-buttom-page-right">
+          <h4 className="main-detail-doctor-about">درباره پزشک</h4>
+          <div className="bio-doctor">
+            <div className="bio-doctor-text">
+              <h5>بیوگرافی:</h5>
+
+              {diseaseArr.map((item) => {
+                return (
+                  <>
+                    <div className="bio-item-doctor">
+                      <CheckBoxIcon style={{ color: "green" }}></CheckBoxIcon>
+                      <p>{item.title}</p>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="detail-docor-buttom-page-left">
+          <h4 className="main-detail-doctor-about"> ثبت درخواست </h4>
+          <div
+            className="detail-doctor-req"
+            style={{
+              backgroundColor: "white",
+            }}
+          >
+            <p className="detail-doctor-req-text">
+              در صورتی که توسط دکتر ویزیت شده اید و تمایل به دریافت نسخه خود
+              دارید، نخست باید برای پزشک مورد نظر خود درخواست ثبت کنید.
+            </p>
+            <button className="detail-doctor-req-button">
+              ثبت درخواست برای {name}
+            </button>
+          </div>
+
+          <div
+            className="detail-doctor-req detail-doctor-rate "
+            style={{
+              backgroundColor: "white",
+            }}
+          >
+            <h5 className="loc-h5-text text-rate-top">میزان رضایت از پزشک</h5>
+
+            <div className="rate-div-deatail-doc">
+              <i
+                className="mdi mdi-star f-19"
+                style={{ marginLeft: "2px" }}
+              ></i>
+
+              {rate}
             </div>
 
-            <div className="detail-docor-buttom-page">
-              <div className="detail-docor-buttom-page-right">
-                <h4 className="main-detail-doctor-about">درباره پزشک</h4>
-                <div className="bio-doctor">
-                  <div className="bio-doctor-text">
-                    <h5>بیوگرافی:</h5>
+            <button
+              className="detail-doctor-req-button"
+              onClick={handleClickOpen}
+            >
+              {" "}
+              رای دادن به {name}
+            </button>
 
-                    {diseaseArr.map((item) => {
-                      return (
-                        <>
-                          <div className="bio-item-doctor">
-                            <CheckBoxIcon
-                              style={{ color: "green" }}
-                            ></CheckBoxIcon>
-                            <p>{item.title}</p>
-                          </div>
-                        </>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-              <div className="detail-docor-buttom-page-left">
-                <h4 className="main-detail-doctor-about"> ثبت درخواست </h4>
-                <div
-                  className="detail-doctor-req"
-                  style={{
-                    backgroundColor: "white",
-                  }}
-                >
-                  <p className="detail-doctor-req-text">
-                    در صورتی که توسط دکتر ویزیت شده اید و تمایل به دریافت نسخه
-                    خود دارید، نخست باید برای پزشک مورد نظر خود درخواست ثبت
-                    کنید.
-                  </p>
-                  <button className="detail-doctor-req-button">
-                    ثبت درخواست برای {name}
-                  </button>
-                </div>
-
-                <div
-                  className="detail-doctor-req detail-doctor-rate "
-                  style={{
-                    backgroundColor: "white",
-                  }}
-                >
-                  <h5 className="loc-h5-text text-rate-top">
-                    میزان رضایت از پزشک
-                  </h5>
-
-                  <div className="rate-div-deatail-doc">
-                    <i
-                      className="mdi mdi-star f-19"
-                      style={{ marginLeft: "2px" }}
-                    ></i>
-
-                    {rate}
-                  </div>
-
-                  <button className="detail-doctor-req-button">
-                    {" "}
-                    رای دادن به {name}
-                  </button>
-                </div>
-              </div>
-            </div>
-          </>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">
+                {"Use Google's location service?"}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Let Google help apps determine location. This means sending
+                  anonymous location data to Google, even when no apps are
+                  running.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Disagree</Button>
+                <Button onClick={handleClose} autoFocus>
+                  Agree
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
