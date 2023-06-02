572fd4630078658c3828a379a33ed0258dcc29fc
Faeze <112781265+faz-7@users.noreply.github.com>
2023-06-02 19:32:37 +0330

    change some models

--- backend/appointments/models.py
+++ backend/appointments/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from accounts.models import Patient, Psychologist
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
 class Request(models.Model):
     sender = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='sender_request')
     receiver = models.ForeignKey(Psychologist, on_delete=models.CASCADE, related_name='receiver_request')
@@ -29,21 +34,18 @@ class Session(models.Model):
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
     medical_recorde = models.ForeignKey(MedicalRecorder, on_delete=models.CASCADE, related_name='medical_recorde')
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    # patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    audio = models.FileField(upload_to=user_directory_path, blank=True, null=True)
 
     def __str__(self):
-        return f'title:{self.title} content:{self.content} patient:{self.patient}'
-
-
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+        return f'title:{self.title} content:{self.content}'
 
 
 class Prescription(models.Model):
-    voice_field = models.FileField(upload_to=user_directory_path, blank=True, null=True)
+    # voice_field = models.FileField(upload_to=user_directory_path, blank=True, null=True)
     session = models.OneToOneField(Session, on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
+
     def __str__(self):
         return f'content:{self.content}'
