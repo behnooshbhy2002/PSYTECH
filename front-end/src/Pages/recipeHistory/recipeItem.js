367bc4f20fa9039542d75d71bfc8ef22c1ff5157
behnooshbhy2002 <behnoosh.bhy@gmail.com>
2023-06-02 23:13:35 +0330

    api for prescreption

--- front-end/src/Pages/recipeHistory/recipeItem.js
+++ front-end/src/Pages/recipeHistory/recipeItem.js
@@ -1,27 +1,28 @@
-import React, { useState } from 'react'
-import './recipeItem.css'
-function RecipeItem() {
-  const [title,setTitle]=useState("");
-  const [date,setData]=useState("");
-  const [text,setText]=useState("");
+import React, { useState } from "react";
+import "./recipeItem.css";
+function RecipeItem(props) {
+  //const [title,setTitle]=useState("");
+  //const [date,setData]=useState("");
+  //const [text,setText]=useState("");
 
+  let title = props?.data?.title;
+  let pre = props?.data?.pre;
+  let date = props?.data?.date;
 
   return (
     <div className="card-wrapper">
       <div className="card-body">
-        <div className='front-card'>
+        <div className="front-card">
           <br></br>
           <h3>جلسه 1</h3>
           <h5>19/02/1402</h5>
         </div>
-        <div className='back-card'>
-          <p>مظالعه کتاب مردی به نام اوه ،
-            توجه به احساسات و....
-          </p>
+        <div className="back-card">
+          <p>مظالعه کتاب مردی به نام اوه ، توجه به احساسات و....</p>
         </div>
       </div>
     </div>
   );
 }
 
-export default RecipeItem
+export default RecipeItem;
