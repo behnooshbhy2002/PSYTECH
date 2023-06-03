26e6ddd6e85413b9b89d90cdacaaf3b21238e6a7
behnooshbhy2002 <behnoosh.bhy@gmail.com>
2023-06-03 10:27:10 +0330

    hiii

--- front-end/src/Pages/CaseHistory.js
+++ front-end/src/Pages/CaseHistory.js
@@ -5,7 +5,8 @@ import CaseInput from "../Components/componentss/CaseInput";
 import CaseItem from "../Components/componentss/CaseItem";
 import "../Components/style/CaseHistory.css";
 import { useLocation } from "react-router-dom";
-
+import Message from "../Components/Error&Loading/Message";
+import { useSelector } from "react-redux";
 function CaseHistory() {
   const [StatusForm, setStatusForm] = useState(false);
   const [fileId, setFileId] = useState(-1);
@@ -13,10 +14,10 @@ function CaseHistory() {
 
   const location = useLocation();
   let FileList = location.state;
-  console.log((FileList))
-  let session_list = FileList?.session_list
-  let medical_recorde = FileList?.medical_recorde?.id
-  console.log(session_list)
+  console.log(FileList);
+  let session_list = FileList?.session_list;
+  let medical_recorde = FileList?.medical_recorde?.id;
+  console.log(session_list);
   // FileList = [
   //   {
   //     id: 1,
@@ -42,43 +43,55 @@ function CaseHistory() {
     setStatusForm(false);
   };
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
   return (
-    <div className="case-history" dir="rtl">
-      <h1 className="text-case-history">شرح حال</h1>
-      <div className="general-status-div">
-        <h4 className="text-case-history">شرح کلی:</h4>
-        <p className="text-case-history">
-          بیمار دارای افسردگی خفیف همراه با اضطراب اجتماعی است که ریشه در کوکی
-          وی دارد و....................
-        </p>
-      </div>
-      <div className="case-list-wrapper" dir="rtl">
-        <div className="caseItem-wrapper">
-          <IoAddCircle
-            id="icon-add-caseItem"
-            size={70}
-            color="#a8a8a9"
-            onClick={() => {
-              //setFileId(false);
-              handleClick(-1);
-            }}
-          ></IoAddCircle>
+    <>
+      {!userInfo ? (
+        <div>
+          <Message>دسترسی غیرمجاز</Message>
         </div>
-        {session_list?.map((item) => {
-          return <CaseItem data={item} clickhandler={handleClick}></CaseItem>;
-        })}
-      </div>
-      <div>
-        {StatusForm && (
-          <CaseInput
-            closing={closeHandle}
-            fileId={fileId}
-            FileList={session_list}
-            medical_recorde={medical_recorde}
-          ></CaseInput>
-        )}
-      </div>
-    </div>
+      ) : (
+        <div className="case-history" dir="rtl">
+          <h1 className="text-case-history">شرح حال</h1>
+          <div className="general-status-div">
+            <h4 className="text-case-history">شرح کلی:</h4>
+            <p className="text-case-history">
+              بیمار دارای افسردگی خفیف همراه با اضطراب اجتماعی است که ریشه در
+              کوکی وی دارد و....................
+            </p>
+          </div>
+          <div className="case-list-wrapper" dir="rtl">
+            <div className="caseItem-wrapper">
+              <IoAddCircle
+                id="icon-add-caseItem"
+                size={70}
+                color="#a8a8a9"
+                onClick={() => {
+                  //setFileId(false);
+                  handleClick(-1);
+                }}
+              ></IoAddCircle>
+            </div>
+            {session_list?.map((item) => {
+              return (
+                <CaseItem data={item} clickhandler={handleClick}></CaseItem>
+              );
+            })}
+          </div>
+          <div>
+            {StatusForm && (
+              <CaseInput
+                closing={closeHandle}
+                fileId={fileId}
+                FileList={session_list}
+                medical_recorde={medical_recorde}
+              ></CaseInput>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
